5bbf8f05d88393b91dcf76dde1ef5d295467a24f
Killian LE VAILLANT <killian.levaillant@exfabrica.io>
2022-12-14 15:28:10 +0100

    Openfoodfact product_name

--- GraphQl/Schema/Mutation/Mutation.cs
+++ GraphQl/Schema/Mutation/Mutation.cs
@@ -1,6 +1,7 @@
 ﻿using GraphQl.Schema.Query;
 using HotChocolate;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Text;
@@ -33,6 +34,20 @@ namespace GraphQl.Schema.Mutation
             }
             var newProduct = await response.Content.ReadFromJsonAsync<ProductResult>();
 
+            if (newProduct.Name == null || newProduct.Name == "")
+            {
+                var pathOpenfoodfacts = "https://world.openfoodfacts.org/api/v2/search?code=" + newProduct.Code + "&fields=code,product_name";
+                var responseOpenfoodfact = await client.GetAsync(pathOpenfoodfacts);
+                if (responseOpenfoodfact.IsSuccessStatusCode)
+                {
+                    var productOpenfoodfact = await responseOpenfoodfact.Content.ReadFromJsonAsync<openfoodfactsProduct>();
+                    if (productOpenfoodfact.products.FirstOrDefault() != null)
+                    {
+                        newProduct.Name = productOpenfoodfact.products.FirstOrDefault().product_name;
+                    }
+                }
+            }
+
             return newProduct;
         }
 
@@ -52,6 +67,20 @@ namespace GraphQl.Schema.Mutation
                 throw new GraphQLException(new Error("error", "error"));
             }
             var updateProduct = await response.Content.ReadFromJsonAsync<ProductResult>();
+
+            if (updateProduct.Name == null || updateProduct.Name == "")
+            {
+                var pathOpenfoodfacts = "https://world.openfoodfacts.org/api/v2/search?code=" + updateProduct.Code + "&fields=code,product_name";
+                var responseOpenfoodfact = await client.GetAsync(pathOpenfoodfacts);
+                if (responseOpenfoodfact.IsSuccessStatusCode)
+                {
+                    var productOpenfoodfact = await responseOpenfoodfact.Content.ReadFromJsonAsync<openfoodfactsProduct>();
+                    if (productOpenfoodfact.products.FirstOrDefault() != null)
+                    {
+                        updateProduct.Name = productOpenfoodfact.products.FirstOrDefault().product_name;
+                    }
+                }
+            }
 
             return updateProduct;
         }
